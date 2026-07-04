c1fa6af36e88a9982b8c5da4af72fcf49f704b71
abhinext2244 <abhisheknextabhi@gmail.com>
2026-07-04 16:43:10 +0530

    feat(auth): add JWT and token utilities

---
+++ learnflow/src/lib/auth/jwt.ts
@@ -0,0 +1,39 @@
+
+import jwt, { SignOptions } from "jsonwebtoken";
+import crypto from "crypto";
+export interface JwtPayload {
+    Id: string; 
+    role: string;
+}
+export interface RefreshTokenPayload {
+    Id: string;
+}
+interface RandomToken {
+    rawToken: string;
+    hashedToken: string;
+}
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET as string;
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET as string;
+const ACCESS_TOKEN_EXPIRATION = process.env.ACCESS_TOKEN_EXPIRATION as SignOptions["expiresIn"];
+const REFRESH_TOKEN_EXPIRATION = process.env.REFRESH_TOKEN_EXPIRATION as SignOptions["expiresIn"];
+
+export const generateAccessToken = (payload: JwtPayload): string => {
+    return jwt.sign({payload}, ACCESS_TOKEN_SECRET, { expiresIn: ACCESS_TOKEN_EXPIRATION });
+};
+
+export const verifyAccessToken = (token: string): JwtPayload => {
+    return jwt.verify(token, ACCESS_TOKEN_SECRET) as JwtPayload;
+};
+export const generateRefreshToken = (payload: RefreshTokenPayload): string => {
+    return jwt.sign({payload}, REFRESH_TOKEN_SECRET, { expiresIn: REFRESH_TOKEN_EXPIRATION });
+};
+
+export const verifyRefreshToken = (token: string): RefreshTokenPayload => {
+    return jwt.verify(token, REFRESH_TOKEN_SECRET) as RefreshTokenPayload;
+};
+
+export const generateRandomToken = (): RandomToken => {
+    const rawToken = crypto.randomBytes(32).toString("hex");
+    const hashedToken = crypto.createHash("sha256").update(rawToken).digest("hex");
+    return {rawToken, hashedToken};
+};
