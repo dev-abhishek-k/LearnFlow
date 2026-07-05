bb64bc675fb860d21df3b87966aead5a08d6fe2b
abhinext2244 <abhisheknextabhi@gmail.com>
2026-07-05 17:21:10 +0530

    feat(auth): add user repository methods

---
+++ learnflow/src/modules/auth/auth.repository.ts
@@ -0,0 +1,25 @@
+import {prisma} from "@/lib/prisma";
+import type {Prisma,User} from "@/generated/prisma/client";
+export class AuthRepository {
+    async findUserByEmail(email: string): Promise<User | null>  {
+        return await prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
+    }
+    async createUser(data:Prisma.UserCreateInput): Promise<User> {
+        return await prisma.user.create({
+            data
+        });
+    }
+    async findUserById(id: string): Promise<User | null> {
+        return await prisma.user.findUnique({
+            where: {
+                id
+            }
+        });
+    }
+}
+            
+export const authRepository = new AuthRepository();
