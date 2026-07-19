decf71afdca7d8edee0adf4bd72e81198926834b
abhinext2244 <abhisheknextabhi@gmail.com>
2026-07-19 17:05:03 +0530

    refactor(section): include course relation in repository

--- learnflow/src/modules/section/section.repository.ts
+++ learnflow/src/modules/section/section.repository.ts
@@ -1,22 +1,37 @@
-import {Prisma,Section} from "@/generated/prisma/client"
-import {prisma} from "@/lib/prisma"
+import { Prisma, Section } from "@/generated/prisma/client";
+import { prisma } from "@/lib/prisma";
 
-
-class SectionRepository{
-    async createSection(data:Prisma.SectionUncheckedCreateInput):Promise<Section>{
-        return prisma.section.create({data})
-    }
-    async getAllSection():Promise<Section[]>{
-        return prisma.section.findMany()
-    }
-    async getSectionById(id:string):Promise<Section | null>{
-        return prisma.section.findUnique({where:{id}})
-    }
-    async updateSection(id:string,data:Prisma.SectionUpdateInput):Promise<Section | null>{
-        return prisma.section.update({where:{id},data})
-    }
-    async deleteSection(id:string):Promise<Section>{
-        return prisma.section.delete({where:{id}})
-    }
+class SectionRepository {
+  async createSection(
+    data: Prisma.SectionUncheckedCreateInput,
+  ): Promise<Section> {
+    return prisma.section.create({ data });
+  }
+  async getAllSection(): Promise<Section[]> {
+    return prisma.section.findMany();
+  }
+  async getSectionById(id: string) {
+    return prisma.section.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        course: {
+          select: {
+            teacherId: true,
+          },
+        },
+      },
+    });
+  }
+  async updateSection(
+    id: string,
+    data: Prisma.SectionUpdateInput,
+  ): Promise<Section | null> {
+    return prisma.section.update({ where: { id }, data });
+  }
+  async deleteSection(id: string): Promise<Section> {
+    return prisma.section.delete({ where: { id } });
+  }
 }
-export const sectionRepository = new SectionRepository
+export const sectionRepository = new SectionRepository();
